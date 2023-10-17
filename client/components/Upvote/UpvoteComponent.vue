5cee2214cabcadba2ed850481416b1c93819f855
Sonia Uwase <usonia@mit.edu>
2023-10-17 14:55:46 -0400

    downvote included

--- client/components/Upvote/UpvoteComponent.vue
+++ client/components/Upvote/UpvoteComponent.vue
@@ -1,25 +1,40 @@
 <script setup lang="ts">
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { ref } from "vue";
+import { storeToRefs } from "pinia";
+import { useUserStore } from "@/stores/user";
 import { fetchy } from "../../utils/fetchy";
 
 const props = defineProps(["post"]);
 let upvotes = (await fetchy(`api/posts/${props.post._id}/upvotes`, "GET")).upvotes;
 let upvoteCount = ref(upvotes.length);
+const { currentUsername } = storeToRefs(useUserStore());
 
 const upvotePost = async () => {
   try {
     await fetchy(`api/posts/${props.post._id}/upvotes`, "POST");
+    upvoteCount.value++;
   } catch {
+    //    try {
+    //      await removeUpvote(upvotes, currentUsername);
+    //      upvoteCount.value--;
+    //    } catch {
+    //      return;
+    //    }
+    await removeUpvote(upvotes, currentUsername);
+    upvoteCount.value--;
     return;
   }
-  upvoteCount.value++;
 };
+
+async function removeUpvote(upvotes, currentUsername) {
+  let userUpvote = upvotes.filter((upvote) => upvote.upvoter === currentUsername.value)[0];
+  await fetchy(`api/upvotes/${userUpvote.upvote_id}`, "DELETE");
+}
 </script>
 
 <template>
   <div>
-    <font-awesome-icon icon="fa-solid fa-thumbs-up" @click="upvotePost" />
+    <!--<font-awesome-icon icon="fa-solid fa-thumbs-up" @click="upvotePost" /> -->
     <button @click="upvotePost">Upvote</button>
     <span>{{ upvoteCount }}</span>
   </div>
