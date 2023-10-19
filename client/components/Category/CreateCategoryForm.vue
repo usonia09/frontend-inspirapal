1c0c236c17506a13f5f5264900906a943581ab6c
Sonia Uwase <usonia@mit.edu>
2023-10-19 10:18:22 -0400

    create category

---
+++ client/components/Category/CreateCategoryForm.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { fetchy } from "../../utils/fetchy";
+import { ref } from "vue";
+
+const name = ref("");
+const createCategory = async (name: string) => {
+  try {
+    await fetchy("api/categories", "POST", {
+      body = { name },
+    });
+  } catch {
+    return;
+  }
+};
+</script>
+
+<template>
+  <form @submit.prevent="createCategory(name)">
+    <textarea id="category-name" v-model="name" placeholder="Add a Category!" required> </textarea>
+    <button type="submit" class="pure-button-primary pure-button">Create Category</button>
+  </form>
+</template>
+
+<style scoped></style>
