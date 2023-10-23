1c844f06aff6f7fe6070c78e9999d6c74ac02b58
Sonia Uwase <usonia@mit.edu>
2023-10-23 14:17:03 -0400

    joining space automatically

--- client/views/ConnectSpaceView.vue
+++ client/views/ConnectSpaceView.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
+import { fetchy } from "@/utils/fetchy";
+import { onBeforeMount } from "vue";
 import MessageListComponent from "../components/ConnectSpace/MessageListComponent.vue";
 
 const props = defineProps(["spaceId", "spaceName"]);
+
+onBeforeMount(async () => {
+  await fetchy(`/api/connectspaces/${props.spaceId}/join`, "PATCH");
+});
 </script>
 
 <template>
