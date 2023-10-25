05f51677021906b9fd99229ff2bf889d1af95839
Sonia Uwase <usonia@mit.edu>
2023-10-25 10:22:13 -0400

    join added to space

--- client/components/ConnectSpace/ConnectSpaceComponent.vue
+++ client/components/ConnectSpace/ConnectSpaceComponent.vue
@@ -14,10 +14,37 @@ function navigateToConnectSpaceView() {
 </script>
 
 <template>
-  <button @click="navigateToConnectSpaceView"><font-awesome-icon icon="comments" />{{ props.space.topic }}</button>
+  <div class="space-item">
+    <span><font-awesome-icon icon="comments" />{{ props.space.topic }}</span>
+    <button @click="navigateToConnectSpaceView">Join</button>
+  </div>
 </template>
 
 <style scoped>
+.space-item {
+  display: flex;
+  align-items: center;
+  margin-top: 10px;
+  justify-content: space-between;
+}
+span svg {
+  margin-right: 10px;
+}
+button {
+  margin-right: 10px;
+  border-color: transparent;
+  background-color: rgb(28, 99, 170);
+  cursor: pointer;
+  padding: 5px;
+  border-radius: 5px;
+  color: white;
+}
+
+button:hover {
+  background-color: rgb(5, 74, 144);
+}
+/* 
+
 button {
   display: flex;
   margin-top: 10px;
@@ -34,5 +61,5 @@ button svg {
 
 button:hover {
   background-color: var(--base-bg);
-}
+} */
 </style>
