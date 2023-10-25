2bf7248c3b7ade0f36d0522b11eddfd623e6c74d
Sonia Uwase <usonia@mit.edu>
2023-10-25 12:21:14 -0400

    label made dropdown

--- client/components/Post/CreatePostForm.vue
+++ client/components/Post/CreatePostForm.vue
@@ -26,15 +26,28 @@ const emptyForm = () => {
 
 <template>
   <form @submit.prevent="createPost(content, label)">
-    <label for="content">Post Contents:</label>
+    <label for="content"><b>Post Contents:</b></label>
     <textarea id="content" v-model="content" placeholder="Create a post!" required> </textarea>
-    <label for="content">Post Label:</label>
+    <p>Post Category</p>
+    <label for="content">Common categories:</label>
+    <select name="label" id="label" v-model="label">
+      <option value="Photography">Photography</option>
+      <option value="Drawing">Drawing</option>
+      <option value="Painting">Painting</option>
+      <option value="Poetry">Poetry</option>
+      <option value="Dance">Dance</option>
+      <option value="Cooking">Cooking</option>
+    </select>
+    <label for="content">Create a new category:</label>
     <input id="label" v-model="label" required />
     <button type="submit" class="pure-button-primary pure-button">Create Post</button>
   </form>
 </template>
 
 <style scoped>
+p {
+  font-weight: bold;
+}
 form {
   background-color: rgb(143, 139, 139);
   border-radius: 1em;
